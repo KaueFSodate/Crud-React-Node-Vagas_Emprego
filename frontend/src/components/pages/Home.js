2ad480355e18fd348a3a15d98a12a3fe3418e590
Kaue Felipe Sodate <99609927+KaueFSodate@users.noreply.github.com>
2023-05-05 18:06:28 -0300

    Update Home.js

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -5,37 +5,48 @@ import { Link } from "react-router-dom";
  
   
   function Home() {
-    const [vagas, setVagas] = useState([])
+  const [vagas, setVagas] = useState([]);
+  const [buscarQuery, setbuscarQuery] = useState("");
 
-    // Pegar os produtos da API
-    useEffect(() => {
-        api.get('/vagas/').then((response) => {
-          setVagas(response.data)
-          console.log(vagas)
-        })
-    }, [])
-    return (
-      <div>
-        <h1>Vagas disponiveis</h1>
-        <p>Veja os detalhes de cada uma</p>
+  useEffect(() => {
+    api.get("/vagas/").then((response) => {
+      setVagas(response.data);
+    });
+  }, []);
 
-        {vagas.length > 0 &&
-                vagas.map((vaga) => (
-                    <div key={vaga.id}>
-                        <h3>{vaga.nome}</h3>
-                        <p>
-                            Descrição: {vaga.descricao}
-                        </p>
-                        <Link to={`/vagas/${vaga.id}`}>Mais detalhes</Link>
-                    </div>         
-        ))}
-        {vagas.length === 0 && (
-                <p>Não há vagas cadastradas ou disponíveis!</p>
-        )}
-      </div>
+  const filteredVagas = vagas.filter((vaga) =>
+    vaga.nome.toLowerCase().includes(buscarQuery.toLowerCase())
+  );
 
-
-    )
+  function handleSearch(e) {
+    setbuscarQuery(e.target.value);
   }
+
+  return (
+    <div>
+      <h1>Vagas disponiveis</h1>
+      <p>Veja os detalhes de cada uma</p>
+
+      <input
+        type="text"
+        placeholder="Pesquisar vagas"
+        value={buscarQuery}
+        onChange={handleSearch}
+      />
+
+      {filteredVagas.length > 0 &&
+        filteredVagas.map((vaga) => (
+          <div key={vaga.id}>
+            <h3>{vaga.nome}</h3>
+            <p>Descrição: {vaga.descricao}</p>
+            <Link to={`/vagas/${vaga.id}`}>Mais detalhes</Link>
+          </div>
+        ))}
+      {filteredVagas.length === 0 && (
+        <p>Não há vagas cadastradas ou disponíveis!</p>
+      )}
+    </div>
+  );
+}
   
   export default Home
